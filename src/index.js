07053e8b898ccbca5b980a400ef083cfc8172df3
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-04-10 14:47:30 -0600

    update index.js to import current script files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 // Styles
 import "./Styles/main-styles.css";
+
+// Scripts
+import "./Scripts/ship-class/ship";
+import "./Scripts/game-board-class/game-board";
+import "./Scripts/player-class/player";
