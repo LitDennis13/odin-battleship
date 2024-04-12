88e813919d43c6f3db93d13e80690d740ddb1bbc
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-04-12 15:35:41 -0600

    update imports for index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./Scripts/utility-functions/utility-functions";
 import "./Scripts/ship-class/ship";
 import "./Scripts/game-board-class/game-board";
 import "./Scripts/player-class/player";
-import "./Scripts/game-module-vsComputer";
+import "./Scripts/game-module";
 import "./Scripts/ship-placement-module";
 import "./Scripts/home-screen";
 import "./Scripts/game-display-module";
